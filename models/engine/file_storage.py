36123a70fa4adbc90af7ca1806afb4b2e5af3a25
kriverao98 <riveraocasiokoral29@gmail.com>
2024-02-28 11:07:46 -0400

    first commit, task5

--- models/engine/file_storage.py
+++ models/engine/file_storage.py
@@ -5,7 +5,9 @@ import json
 from base_model import BaseModel
 
 class FileStorage:
+    def __FileStorage__(self):
+        self.__FileStorage__ = FileStorage
     base_ptr = BaseModel
     json(to_dict(base_ptr))
-    with open("base_ptr.json", "r+")
-    json.load
+    with open("base_ptr.json", "r+") as file:
+        data = json.load
