6403c8e657b6fa29961ae52d52d09d9c6e4c64bf
Ninoshka Perez <perez.ninoshka31@gmail.com>
2024-02-28 23:48:13 -0400

    Create console.py

---
+++ console.py
@@ -0,0 +1,93 @@
+#!/usr/bin/python3
+"""The console for holbertonschool-AirBnB_clone project"""
+
+
+import cmd
+import sys
+import models
+from models.base_model import BaseModel
+from models.user import User
+from models.state import State
+from models.city import City
+from models.amenity import Amenity
+from models.place import Place
+from models.review import Review
+
+classes = {"BaseModel": BaseModel, "User": User, "State": State, "City": City,
+           "Amenity": Amenity, "Place": Place, "Review": Review}
+
+
+class HBNBCommand(cmd.Cmd):
+    """
+    our cmd loop interpreter
+    """
+    prompt = '(hbnb) '
+
+    def do_quit(self, args):
+        """Quit command to exit the program\n"""
+        return True
+
+    def do_EOF(self, args):
+        """Quit console at EOF\n"""
+        print("")
+        return True
+
+    def emptyline(self):
+        """No action: input is empty line + ENTER\n"""
+        pass
+
+    def default(self, args):
+        """Handle cmd in format <class name>.cmd()\n"""
+        arg = args.split(".")
+        if len(arg) > 1:
+            if arg[0] in classes:
+                if arg[1] == "all()":
+                    self.do_all(arg[0])
+                    return
+                if arg[1] == "count()":
+                    self.do_count(arg[0])
+                    return
+                command = arg[1].split("(")
+                command[1] = command[1].replace(")", "")
+                if command[0] == "show" or command[0] == "destroy":
+                    command[1] = command[1].strip('\"')
+                    new_cmd = arg[0] + " " + command[1]
+                    if command[0] == "show":
+                        self.do_show(new_cmd)
+                    else:
+                        self.do_destroy(new_cmd)
+                    return
+                if command[0] == "update":
+                    command[1] = command[1].replace("{", "")
+                    command[1] = command[1].replace("}", "")
+                    command[1] = command[1].replace(".", "")
+                    command[1] = command[1].replace('"', '')
+                    command[1] = command[1].replace("'", "")
+                    detail = command[1].split(", ")
+                    if ": " in command[1]:
+                        objId = detail[0]
+                        for i in range(len(detail)):
+                            if ": " in detail[i]:
+                                upDate = detail[i].split(": ")
+                                attrName = upDate[0]
+                                attrValue = upDate[1]
+                                new_cmd = arg[0] + " " + detail[0]\
+                                    + " " + attrName + " " + attrValue
+                                self.do_update(new_cmd)
+                            i += 1
+                        return
+                    else:
+                        new_cmd = arg[0] + " " + detail[0]\
+                            + " " + detail[1] + " " + detail[2]
+                        self.do_update(new_cmd)
+                        return
+                return cmd.Cmd.default(self, args)
+            else:
+                print("** class doesn't exist **")
+                return False
+        else:
+            return cmd.Cmd.default(self, args)
+
+
+if __name__ == '__main__':
+    HBNBCommand().cmdloop()
