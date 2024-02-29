83a594d9764c52451ae882d2c64d1f4fbe7393c4
Ninoshka Perez <perez.ninoshka31@gmail.com>
2024-02-28 23:58:59 -0400

    Create state.py

---
+++ models/state.py
@@ -0,0 +1,13 @@
+#!/usr/bin/python3
+"""class State"""
+
+from models.base_model import BaseModel
+
+
+class State(BaseModel):
+    """
+        State class for the Airbnb project
+        with public class attribute
+    """
+
+    name = ""
